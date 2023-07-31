110525fd1aaac4ddf1300aa68ff48220acd8564e
Opemipo <alomajaopemipo8@gmail.com>
2023-07-31 22:01:33 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { MainSidebarComponent } from './components/main-sidebar/main-sidebar.component';
+import { ContentWrapperComponent } from './components/content-wrapper/content-wrapper.component';
+import { ControlSidebarComponent } from './components/control-sidebar/control-sidebar.component';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    ContentWrapperComponent,
+    ControlSidebarComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
